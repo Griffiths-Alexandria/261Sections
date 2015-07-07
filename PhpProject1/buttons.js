c81585be211f5e23f1eb43d5ceca6e59b69faf31
Griffiths-Alexandria <alexcgriff@gmail.com>
2015-07-07 10:26:37 -0700

    updated

---
+++ PhpProject1/buttons.js
@@ -0,0 +1,35 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+function userQuery(){
+
+    //query_url = "http://api.yelp.com/v2/search/";
+    
+    loc = "location=" + document.getElementById("loc").value;
+
+    terms = document.forms[0];
+    term = "";
+    i = 0;
+    
+    for(i; i<terms.length; i++){
+        if(terms[i].checked){
+            term+=terms[i].value; 
+        } 
+    }
+    
+    category = document.forms["category"]["genre"].value;
+    sort = document.forms["sort"]["sort"].value; 
+    radius = document.forms["radius"]["radius"].value; 
+    limit = document.forms["limit"]["limit"].value; 
+    
+    
+    
+   values = loc + term + category + sort + radius + limit;
+   document.getElementById("demo").innerHTML = values;
+   
+}
+
+
